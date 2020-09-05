b3f4c111dd6700824b2e89ca7eaf1f0ea5f271c5
Mr <zack@labs.ws>
2020-09-05 10:42:18 +0200

    Updates

---
+++ precache-manifest.78370af754693eaf69337323265ae870.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9366d446ea802af0b31fec5d20aabf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4f26e123e750e7b1410",
+    "url": "/static/css/main.79f941af.chunk.css"
+  },
+  {
+    "revision": "dde585672e47a4fa38b9",
+    "url": "/static/js/2.821273c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.821273c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4f26e123e750e7b1410",
+    "url": "/static/js/main.17f28039.chunk.js"
+  },
+  {
+    "revision": "8737654714f81665bed6",
+    "url": "/static/js/runtime-main.0bbc0104.js"
+  }
+]);
